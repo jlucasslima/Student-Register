874232d4260949d3690ad2dcb8886ddede15e7e4
João Lucas de Lima Souza <jlucas2140@gmail.com>
2026-03-26 12:06:40 -0300

    Delete TestaAluno.java

--- TestaAluno.java
+++
@@ -1,8 +0,0 @@
-public class TestaAluno {
-    public static void main(String[] args) {
-        Aluno a = new Aluno("Ze da Silva Pereira Antunes", 22, "RA123456", "Engenharia");
-        
-        System.out.println(a);
-        System.out.println("Nome bibliografico: " + a.getNomeBiblio());
-    }
-}
