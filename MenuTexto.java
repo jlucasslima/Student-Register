c691ee4a673e2b6d3d3481878b454114dcd6c24c
João Lucas de Lima Souza <jlucas2140@gmail.com>
2026-03-26 12:05:32 -0300

    Delete MenuTexto.java

--- MenuTexto.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class MenuTexto implements IMenu {
-    @Override
-    public void exibirMenu(CadastroAlunos cadastro) {
-        System.out.println("Menu em texto não implementado completamente nesta versão.");
-        System.out.println("Por favor, utilize o MenuGrafico.");
-    }
-}
